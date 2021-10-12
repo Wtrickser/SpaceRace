373fe962fb59b1dda6180c65c16d6bc73c7038ee
Joseph <JSPenland97@gmai.com>
2021-10-12 18:03:32 -0400

    Sprites added

--- spacerace.py
+++ spacerace.py
@@ -3,6 +3,7 @@ import pygame, sys
 pygame.init()
 screen = pygame.display.set_mode((1280,720))
 clock = pygame.time.Clock()
+pygame.mouse.set_visible(False)
 
 class SpaceShip(pygame.sprite.Sprite):
   def __init__(self,path,x_pos,y_pos,speed):
@@ -28,13 +29,31 @@ spaceship = SpaceShip('spaceship.png',640,500,10)
 spaceship_group = pygame.sprite.GroupSingle()
 spaceship_group.add(spaceship)
 
+class Meteor(pygame.sprite.Sprite):
+  def __init__(self,path,x_pos,y_pos,x_v,y_v):
+    super().__init__()
+    self.image = pygame.image.load("Meteor1.png")
+    self.rect = self.image.get_rect(center = (x_pos,y_pos))
+  
+  def update(self):
+    self.rect
+
+meteor1 = Meteor('Meteor1.png',400,400,1,1)
+meteor_group = pygame.sprite.Group()
+meteor_group.add(meteor1)
+
 while True:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       pygame.quit()
       sys.exit()
+  
   screen.fill((42,45,51))
+  
   spaceship_group.draw(screen)
   spaceship_group.update()
+
+  meteor_group.draw(screen)
+
   pygame.display.update()
   clock.tick(120)
