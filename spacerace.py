978e0eae83e47b1c6cfd64a169c06f62d437dd19
Joseph <JSPenland97@gmai.com>
2021-10-16 18:52:49 -0400

    lasers added

--- spacerace.py
+++ spacerace.py
@@ -6,11 +6,10 @@ clock = pygame.time.Clock()
 pygame.mouse.set_visible(False)
 
 class SpaceShip(pygame.sprite.Sprite):
-  def __init__(self,path,x_pos,y_pos,speed):
+  def __init__(self,path,x_pos,y_pos):
     super().__init__()
     self.image = pygame.image.load(path)
     self.rect = self.image.get_rect(center = (x_pos,y_pos))
-
   def update(self):
     self.rect.center = pygame.mouse.get_pos()
     self.screen_constrain()
@@ -25,7 +24,7 @@ class SpaceShip(pygame.sprite.Sprite):
     if self.rect.bottom >= 720:
       self.rect.bottom = 720
 
-spaceship = SpaceShip('spaceship.png',640,500,10)
+spaceship = SpaceShip('spaceship.png',640,500)
 spaceship_group = pygame.sprite.GroupSingle()
 spaceship_group.add(spaceship)
 
@@ -36,15 +35,28 @@ class Meteor(pygame.sprite.Sprite):
     self.rect = self.image.get_rect(center = (x_pos,y_pos))
     self.x_v = x_v
     self.y_v = y_v
-  
   def update(self):
     self.rect.centerx += self.x_v
     self.rect.centery += self.y_v
+    if self.rect.centery >= 800:
+      self.kill()
 
 meteor_group = pygame.sprite.Group()
-
 METEOR_EVENT = pygame.USEREVENT
-pygame.time.set_timer(METEOR_EVENT,250)
+pygame.time.set_timer(METEOR_EVENT,100)
+
+class Laser(pygame.sprite.Sprite):
+  def __init__(self,path,pos,speed):
+    super().__init__()
+    self.image = pygame.image.load(path)
+    self.rect = self.image.get_rect(center = pos)
+    self.speed = speed
+  def update(self):
+    self.rect.centery -= self.speed
+    if self.rect.centery <= -100:
+      self.kill()
+
+laser_group = pygame.sprite.Group()
 
 while True:
   for event in pygame.event.get():
@@ -59,8 +71,15 @@ while True:
       random_y_v = random.randrange(4,10)
       meteor = Meteor(meteor_path,random_x_pos,random_y_pos,random_x_v,random_y_v)
       meteor_group.add(meteor)
+    if event.type == pygame.MOUSEBUTTONDOWN:
+      new_laser = Laser('Laser.png', event.pos,15)
+      laser_group.add(new_laser)
+
   screen.fill((42,45,51))
   
+  laser_group.draw(screen)
+  laser_group.update()
+
   spaceship_group.draw(screen)
   spaceship_group.update()
 
